b7bc20c50aa6e98c83d9d7c10ba6b6d6cdbc585b
zfan <zfan1860@gmail.com>
2016-04-18 15:42:02 +0800

    Add ZFLabel.swift

---
+++ ZFLabel.swift
@@ -0,0 +1,44 @@
+//
+//  ZFLabel.swift
+//  swiftTest
+//
+//  Created by Zfan on 4/18/16.
+//  Copyright © 2016 Zfan. All rights reserved.
+//
+
+import UIKit
+
+public enum ZFLabelTextAlignment {
+    case Middle
+    case Top
+    case Bottom
+}
+
+class ZFLabel: UILabel {
+    
+    internal var ZF_verticalTextAlignment : ZFLabelTextAlignment = .Middle
+    
+    override func textRectForBounds(bounds: CGRect, limitedToNumberOfLines numberOfLines: Int) -> CGRect {
+        let rect : CGRect = super.textRectForBounds(bounds, limitedToNumberOfLines: numberOfLines)
+        return rect
+    }
+    
+    override func drawTextInRect(rect: CGRect) {
+        super.drawTextInRect(self.calculateTextRectWithRect(rect))
+    }
+    
+    func calculateTextRectWithRect(rect : CGRect) -> CGRect {
+        var calRect : CGRect;
+        let fitSize = self.sizeThatFits(rect.size)
+        
+        switch ZF_verticalTextAlignment {
+        case .Top:
+            calRect = CGRectMake(0, 0, self.bounds.size.width, fitSize.height);
+        case .Bottom:
+            calRect = CGRectMake(0, rect.size.height - fitSize.height, self.bounds.size.width, fitSize.height);
+        case .Middle:
+            calRect = rect;
+        }
+        return calRect;
+    }
+}
